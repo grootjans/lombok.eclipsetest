403208254dc4a957372e9dc75b4376d30e7febfe
Robbert Jan Grootjans <grootjans@gmail.com>
2012-01-23 19:59:52 +0100

    Added source for the eclipse test framework
    This source was pulled from a branch of the lombok project. Given the
    nature of a eclipse plugin project (which this is) it is hard if
    not impossible to build and run this project next to lombok. Pulling
    it out and putting it into a separate project is the first step in
    making the tests viable again.

---
+++ src/lombok/refactoring/tests/TestAll.java
@@ -0,0 +1,18 @@
+package lombok.refactoring.tests;
+public class TestAll {
+	
+//	/*1: Done :1*/
+//	this
+//	text
+//	can
+//	not
+//	be
+//	executed
+//	and
+//	should
+//	be
+//	commented
+//	out
+//	/*:1:*/
+
+}

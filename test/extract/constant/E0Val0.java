14cbaa1fcc2d6f83e3b425bc351c59613ae37c29
Sander Koning <askoning@gmail.com>
2012-06-29 13:54:14 +0200

    New testcase: extract constant on val:s

---
+++ test/extract/constant/E0Val0.java
@@ -0,0 +1,15 @@
+package E0;
+
+import lombok.val;
+
+public class E0Val0 {
+
+	/*1: ExtractConstant("foo", FOO) :1*/
+	public final val oldName = "foo";
+	/*:1:*/
+	
+	/*2: ExtractConstant(7, SEVEN) :2*/
+	public val oldName2 = 7;
+	/*:2:*/
+
+}

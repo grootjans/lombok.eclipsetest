0c40f6a99cce0d80ef91926c8ef0ba707591195d
Sander Koning <askoning@gmail.com>
2012-06-29 13:53:55 +0200

    Remove bogus test

--- test/extract/constant/E0ConReqArgs1.java
+++ test/extract/constant/E0ConReqArgs1.java
@@ -9,9 +9,5 @@ public class E0ConReqArgs1 {
 	/*1: ExtractConstant(5, CONSTANT) :1*/
 	public final int oldName = 5;
 	/*:1:*/
-
-	/*2: ExtractConstant(oldName2, newfield2) :2*/
-	public int oldName2;
-	/*:2:*/
 	
 }

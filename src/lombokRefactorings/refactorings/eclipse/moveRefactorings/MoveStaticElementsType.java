655ecc990e822bca18031cd5c75579a7344a02ca
Sander Koning <askoning@gmail.com>
2012-06-30 22:14:01 +0200

    Replace one not-working refactoring with another. request.findMembers() expects /*: here :*/ tags, this code makes sure we search for numbered tags instead. However, the refactoring itself still doesn't work - somehow the type cannot be resolved. Something for later.

--- src/lombokRefactorings/refactorings/eclipse/moveRefactorings/MoveStaticElementsType.java
+++ src/lombokRefactorings/refactorings/eclipse/moveRefactorings/MoveStaticElementsType.java
@@ -5,9 +5,12 @@ package lombokRefactorings.refactorings.eclipse.moveRefactorings;
 
 import lombokRefactorings.refactorings.RefactoringUtils;
 import lombokRefactorings.regex.RefactoringRequest;
+import lombokRefactorings.regex.RegexUtilities;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.internal.corext.codemanipulation.CodeGenerationSettings;
 import org.eclipse.jdt.internal.corext.refactoring.structure.MoveStaticMembersProcessor;
 import org.eclipse.jdt.internal.ui.preferences.JavaPreferencesSettings;
@@ -22,10 +25,16 @@ public class MoveStaticElementsType extends AbstractMoveRefactoring{
 
 	@Override
 	public void run(RefactoringRequest request) throws Exception {
-		CodeGenerationSettings settings = JavaPreferencesSettings
-		.getCodeGenerationSettings(request.getCompilationUnit().getJavaProject());
-		IMember[] members = request.findMembers().toArray(new IMember[0]);
-		MoveStaticMembersProcessor processor = new MoveStaticMembersProcessor(members, settings);
+		CodeGenerationSettings settings = JavaPreferencesSettings.getCodeGenerationSettings(request.getCompilationUnit().getJavaProject());
+		
+		ICompilationUnit iCompilationUnit = request.getCompilationUnit();
+		int firstChar = RegexUtilities.findRegex("\\s*?([^\\s])",
+				iCompilationUnit.getSource(),
+				request.getOpeningTagMatcher().end(),
+				request.getClosingTagMatcher().start()).start();
+		IMember member = (IMember) iCompilationUnit.getElementAt(firstChar);
+		
+		MoveStaticMembersProcessor processor = new MoveStaticMembersProcessor(new IMember[] {member}, settings);
 		processor.setDestinationTypeFullyQualifiedName(request.getParameter(0));
 		processor.checkInitialConditions(new NullProgressMonitor());
 		ProcessorBasedRefactoring refactoring = new ProcessorBasedRefactoring(processor);
